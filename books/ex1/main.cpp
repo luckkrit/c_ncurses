5a3763c7e0c38dea63df7f8be3ebbe36d1538110
Krit Chomaitong <luckkrit@hotmail.com>
2026-01-04 22:22:49 +0700

    update example of text attribute

--- books/ex1/main.cpp
+++ books/ex1/main.cpp
@@ -69,16 +69,28 @@ int main()
     //     napms(s);
     // }
 
-    int pieces;
-    const float uni = 4.5;
-    addstr("SHUSHI BAR");
-    move(2,0);
-    printw("We have Uni today for $%.2f.\n", uni);
-    addstr("How many pieces would you like?");
-    refresh();
-    scanw("%d", &pieces);
-    printw("You want %d pieces?\n",pieces);
-    printw("That will be $%.2f!",uni*pieces);
+    // int pieces;
+    // const float uni = 4.5;
+    // addstr("SHUSHI BAR");
+    // move(2,0);
+    // printw("We have Uni today for $%.2f.\n", uni);
+    // addstr("How many pieces would you like?");
+    // refresh();
+    // scanw("%d", &pieces);
+    // printw("You want %d pieces?\n",pieces);
+    // printw("That will be $%.2f!",uni*pieces);
+    // refresh();
+
+    attron(A_BOLD);
+    addstr("Twinkle, twinkle little star\n");
+    attron(A_BLINK);
+    addstr("How I wonder what you are.\n");
+    attroff(A_BOLD);
+    addstr("Up above the world so high, \n");
+    addstr("Like a diamond in the sky.\n");
+    attrset(A_NORMAL);
+    addstr("Twinkle, twinkle little star\n");
+    addstr("How I wonder what you are.\n");
     refresh();
 
     getch();
