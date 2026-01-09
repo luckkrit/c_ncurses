d40a60d3f9f5d1d0337820a8baaa9dbefba6d42f
Krit Chomaitong <luckkrit@hotmail.com>
2026-01-09 22:47:18 +0700

    add acs_character

--- books/ex1/main.cpp
+++ books/ex1/main.cpp
@@ -224,8 +224,24 @@ int main()
     // }
     // refresh();
 
-    // attrset(A_ALTCHARSET);
-    // addstr("Hello there!");
+    // // attrset(A_ALTCHARSET);
+    // // addstr("Hello there!");
+    // refresh();
+
+    // addch(ACS_ULCORNER);
+    // addch(ACS_HLINE);
+    // addch(ACS_URCORNER);
+    // addch('\n');
+
+    // addch(ACS_VLINE);
+    // addch(' ');
+    // addch(ACS_VLINE);
+    // addch('\n');
+
+    // addch(ACS_LLCORNER);
+    // addch(ACS_HLINE);
+    // addch(ACS_LRCORNER);
+    // addch('\n');
     // refresh();
 
     // chtype string[] = {
